eb324281e4933f8dc34ac097e480fe5d714900a0
doanthihuong <doanthihuong>
2022-06-30 10:32:26 +0700

    hien-thi-modal

---
+++ src/main/java/com/example/ajaxproductmangament/service/HouseService.java
@@ -0,0 +1,32 @@
+package com.example.ajaxproductmangament.service;
+
+import com.example.ajaxproductmangament.model.House;
+import com.example.ajaxproductmangament.repository.HouseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class HouseService implements IHouseService{
+  @Autowired
+    HouseRepository houseRepository;
+    @Override
+    public Iterable<House> findAll() {
+        return houseRepository.findAll();
+    }
+
+    @Override
+    public Optional<House> findById(Long id) {
+        return houseRepository.findById(id);
+    }
+
+    @Override
+    public House save(House house) {
+        return houseRepository.save(house);
+    }
+
+    @Override
+    public void remove(Long id) {
+        houseRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/ajaxproductmangament/service/IHouseService.java
@@ -0,0 +1,6 @@
+package com.example.ajaxproductmangament.service;
+
+import com.example.ajaxproductmangament.model.House;
+
+public interface IHouseService extends IGeneralService<House>{
+}
